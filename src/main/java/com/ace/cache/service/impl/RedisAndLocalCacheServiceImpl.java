01f3dfef3e12c48ec6a785540a0d0536e051cc30
bill_ho <bill_ho@foxmail.com>
2020-02-29 21:33:33 +0800

    修改本地缓存绑定请求

--- src/main/java/com/ace/cache/service/impl/RedisAndLocalCacheServiceImpl.java
+++ src/main/java/com/ace/cache/service/impl/RedisAndLocalCacheServiceImpl.java
@@ -1,86 +1,125 @@
 package com.ace.cache.service.impl;
 
-import cn.hutool.cache.Cache;
-import cn.hutool.cache.CacheUtil;
-import cn.hutool.cache.impl.CacheObj;
-import cn.hutool.core.date.DateUnit;
-import cn.hutool.core.date.DateUtil;
-import com.ace.cache.config.RedisConfig;
 import com.ace.cache.service.IBaseCacheService;
 import com.google.common.collect.Sets;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
-import java.util.Date;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
+import java.util.WeakHashMap;
 
 @Service
 public class RedisAndLocalCacheServiceImpl extends RedisServiceImpl implements IBaseCacheService {
-
-    Cache<String,String> localCache;
-    @Autowired
-    private RedisConfig redisConfig;
-    @Autowired
-    public void init(){
-        /**
-         * 超时设置为redis的一半，以激活redis
-         */
-        localCache = CacheUtil.newLRUCache(redisConfig.getLocalCacheSize().intValue(),DateUnit.MINUTE.getMillis()* 60);
-    }
+    private static final String CacheName= "smLocalCache";
+//    @Autowired
+//    private RedisConfig redisConfig;
+//    @Autowired
+//    public void init(){
+//        /**
+//         * 超时设置为redis的一半，以激活redis
+//         */
+//        //localCache = CacheUtil.newLRUCache(redisConfig.getLocalCacheSize().intValue(),DateUnit.MINUTE.getMillis()* 60);
+//
+//    }
 
     @Override
     public String get(String key) {
-        String val = localCache.containsKey(key)? localCache.get(key):
-                super.get(key);
-        return val;
+        if (isEnable()){
+            Map<String,String> cache = localCache();
+            String val = cache.containsKey(key)? cache.get(key):
+                    super.get(key);
+            return val;
+        }
+        else{
+            return  super.get(key);
+        }
+
     }
 
     @Override
     public Set<String> getByPre(String pre) {
-        Set<String> keys = getLocalCacheByPre(pre);
-        Set<String> superKeys = super.getByPre(pre);
-        if (null!= superKeys){
-            keys.addAll(superKeys);
+        if (isEnable()) {
+            Set<String> keys = getLocalCacheByPre(pre);
+            if (null == keys) {
+                return super.getByPre(pre);
+            } else {
+                Set<String> superKeys = super.getByPre(pre);
+                if (null != superKeys&& ! superKeys.isEmpty()) {
+                    keys.addAll(superKeys);
+                }
+                return keys;
+            }
         }
-        return keys;
+        else {
+            return super.getByPre(pre);
+        }
     }
 
     @Override
     public String set(String key, String value) {
-        this.localCache.put(key,value);
-        return super.set(key,value);
+        if (isEnable()) {
+            this.localCache().put(key, value);
+            return super.set(key, value);
+        }
+        else {
+            return super.set(key,value);
+        }
     }
 
     @Override
     public String set(String key, String value, int expire) {
-        this.localCache.put(key,value,expire * DateUnit.MINUTE.getMillis()/12);
-        return super.set(key,value,expire);
+        if (isEnable()){
+            this.localCache().put(key,value);
+            return super.set(key,value,expire);
+        }
+        else {
+            return super.set(key,value,expire);
+        }
     }
 
     @Override
     public Long delPre(String key) {
-        Set<String> keys = getLocalCacheByPre(key);
-        for (String k :
-                keys) {
-            this.localCache.remove(k);
+        if (isEnable()){
+            Set<String> keys = getLocalCacheByPre(key);
+            Map<String,String> map = localCache();
+            for (String k :
+                    keys) {
+                map.remove(k);
+            }
+            return super.delPre(key);
+
         }
-        return super.delPre(key);
+        else
+        {
+            return super.delPre(key);
+        }
     }
 
     @Override
     public Long del(String... keys) {
-        if (keys.length>0){
-            for (String key :
-                    keys) {
-                if (this.localCache.containsKey(key)) {
-                    this.localCache.remove(key);
-                }
-            }
-            return super.del(keys);
-        }
-        return 0L;
+       if (isEnable()){
+           if (keys.length>0){
+               Map<String,String> map = this.localCache();
+               if (!map.isEmpty()) {
+                   for (String key :
+                           keys) {
+                       if (map.containsKey(key)){
+                           map.remove(key);
+                       }
+                   }
+               }
+               return super.del(keys);
+           }
+           return 0L;
+       }
+       else {
+           return super.del(keys);
+       }
     }
 
 //    @Override
@@ -89,7 +128,7 @@ public class RedisAndLocalCacheServiceImpl extends RedisServiceImpl implements I
 //    }
 
     private Set<String> getLocalCacheByPre(String pre){
-        Iterator<CacheObj<String,String>> iterator = localCache.cacheObjIterator();
+        Iterator<Map.Entry<String,String>> iterator = localCache().entrySet().iterator();
         Set<String> set = Sets.newHashSet();
         while (iterator.hasNext()){
             String key = iterator.next().getKey();
@@ -99,4 +138,25 @@ public class RedisAndLocalCacheServiceImpl extends RedisServiceImpl implements I
         }
         return set;
     }
+
+    private boolean isEnable(){
+        ServletRequestAttributes servletContainer = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (null == servletContainer){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    private Map<String,String> localCache(){
+        ServletRequestAttributes servletContainer = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = servletContainer.getRequest();
+        Map<String,String> map =  (Map<String,String>) request.getAttribute(CacheName);
+        if (null == map) {
+            map = new WeakHashMap<>();
+            request.setAttribute(CacheName,map);
+        }
+        return map;
+    }
+
 }
